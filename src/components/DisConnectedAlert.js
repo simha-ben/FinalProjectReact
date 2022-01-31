f250d4d84ade6765aab4259166210d26d69c6acd
simha <s0583239362@gmail.com>
2022-01-31 19:31:36 +0200

    alert 80vh body

--- src/components/DisConnectedAlert.js
+++ src/components/DisConnectedAlert.js
@@ -8,7 +8,7 @@ export default function DisConnectedAlert() {
         nevigate('/Login')
     }
     return (
-        < div>
+        < div style={{height:'72vh'}}>
             <div className="alert alert-danger alert-dismissible">
                 אווופס עדיין לא נכנסת למערכת
                 <a onClick={callbeck} className="close" data-dismiss="alert" aria-label="close"> להתחברות/הרשמה לחץ כאן</a>
